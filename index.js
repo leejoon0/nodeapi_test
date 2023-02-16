a26e108a6387625163b88b876d5ee8129340faba
leejoon0 <leejoon0@gmail.com>
2023-02-16 23:05:46 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/sound/:name", (req, res) => {
+  const p = req.params;
+  console.log(p);
+  const q = req.query;
+  console.log(q);
+
+  res.send({ data: p });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
